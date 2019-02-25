6e65931017b0bb5ccc72725259daba6780797867
Leandro Souza <cartaparaleandro@gmail.com>
2019-02-25 17:34:38 -0300

    authorization in event and project controllers, nested event in project, renders in actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
-  resources :projects
+
+  # events nested to use projects_id
+  resources :projects do
+    resources :events, only: [ :new, :create ]
+  end
+
+  resources :events, only: [ :show, :index, :edit, :update, :destroy]
 end
