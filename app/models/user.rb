7a544bd68e5c951ea51f9ba75633b17866e69810
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-25 16:25:27 -0300

    added artists and contractors ids and belongs/hasmany

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :artist_projects,
+           class_name: 'Project',
+           foreign_key: :artist_id,
+           dependent: :destroy
+
+  has_many :contractor_events,
+           class_name: 'Event',
+           foreign_key: :contractor_id
 end
