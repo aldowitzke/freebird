fa54be4e12e0e23f42ce0211ef379326c61723e9
Leandro Souza <cartaparaleandro@gmail.com>
2019-02-27 11:11:54 -0300

    added simple validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,12 @@ class Project < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :project_genres, dependent: :destroy
   has_many :genres, through: :project_genres
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+
 end
