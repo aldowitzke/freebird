81d5785fee8615f364e9f7d8f8a3007d669f963a
joao gabriel <joaogabrielbarreto1337@gmail.com>
2019-02-28 14:02:59 -0300

    added search bar to the navbar and added typed.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: [" ", "Som de Barzinho", "Mpb", "Rap", "Banda de Casamento", "Rock"],
+  typeSpeed: 40,
+  attr: "placeholder",
+  loop: true
+}
+
+var typed = new Typed(".search-name", options);
