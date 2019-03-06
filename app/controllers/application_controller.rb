8467bb983db30f6dd069eb978b4ad1a07ed53db5
Ian Gigliotti <ian@sala33.com.br>
2019-03-06 14:40:19 -0300

    retirado prepend true do app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, prepend: true
+  protect_from_forgery with: :exception
 
   # configure permitted parameters: access to user params without UsersController
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
