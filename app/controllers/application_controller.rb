fe4bdf496764b4abd7a311797bfdc97b4ca488ee
Ian Gigliotti <ian@sala33.com.br>
2019-03-06 14:26:52 -0300

    removed teste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
   # configure permitted parameters: access to user params without UsersController
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
