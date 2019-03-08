0617ddf67a0681e8ccacf19145c767db5fd126c7
Leandro Souza <cartaparaleandro@gmail.com>
2019-03-08 18:24:22 -0300

    portuguese

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,5 +3,6 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import { Portuguese } from "flatpickr/dist/l10n/pt.js"
 
 flatpickr(".datepicker", {
-  "locale": Portuguese
+  "locale": Portuguese,
+  "dateFormat": "d-m-Y"
 })
