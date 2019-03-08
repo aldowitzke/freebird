2bb1d062f77f41975d85e7c047c5b51198069d33
Ian Gigliotti <ian@sala33.com.br>
2019-03-08 16:08:09 -0300

    Seed and bichices

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,17 +24,17 @@ freebird = User.create!(
    last_name: 'admin',
    email: 'contato@freebird.show',
    admin: true,
-   password: '123456',
+   password: '@123123',
    remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551298452/darth.jpg",
 )
 
 aldo = User.create!(
    first_name: 'Aldo',
    last_name: 'Witzke',
-   email: 'aldolindo@gmail.com',
-   password: '123456',
-   remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551298205/aldo.jpg",
-   phone: "(11)9999-4567",
+   email: 'aldomwa55@gmail.com',
+   password: '123123',
+   remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551462580/v2hnlvpvpzlzy3yb4mg2.jpg",
+   phone: "(11)97644-9229",
    city: "São Paulo",
    state: "SP",
 )
@@ -42,32 +42,32 @@ aldo = User.create!(
  ian = User.create!(
    first_name: 'Ian' ,
    last_name: 'Gigliotti',
-   email: 'ianlindo@gmail.com',
+   email: 'ian@sala33.com.br',
    artist: true,
-   password: '123456',
+   password: '123123',
    remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551877677/IMG_3035.jpg",
    phone: "(14)99789-1234",
    city: "Marília",
    state: "SP",
 )
 
-luciana = User.create!(
+lucianauser = User.create!(
   first_name: 'Luciana',
   last_name: 'Pires',
   email: 'lupires.canta@gmail.com',
   artist: true,
-  password: '123456',
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551451065/luciana.png",
+  password: '123123',
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552062324/lucianapires.png",
   phone: "(14)98159-9492"
   )
 
 kreo = User.create!(
-  first_name: 'Kréo',
+  first_name: 'Krêo',
   last_name: 'Fidélis',
   email: 'kreofidelis@hotmail.com',
   artist: true,
-  password: '123456',
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551451435/kreoperfil.jpg",
+  password: '123123',
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552062476/kreofidelis.jpg",
   phone: "(14)99761-3196"
   )
 
@@ -76,87 +76,83 @@ puts 'Creating categories'
 casamento = Category.create!(name: 'Casamento')
 cover = Category.create!(name: 'Cover')
 banquinho = Category.create!(name: 'Banquinho e Violão')
-pauleira = Category.create!(name: 'Rock Pauleira')
-dancar = Category.create!(name: 'Swinguera')
-fuleragem = Category.create!(name: 'Fuleragem')
-solo = Category.create!(name: 'Artista Solo')
+autoral = Category.create!(name: 'Show Autoral')
+tributo = Category.create!(name: 'Tributo')
+banda = Category.create!(name: 'Banda')
+infantil = Category.create!(name: 'Infantil')
+churrasco = Category.create!(name: 'Churrasco')
+corporativo = Category.create!(name: 'Corporativo')
+formatura = Category.create!(name: 'Formatura')
+roda = Category.create!(name: 'Roda de Samba')
 
 puts 'Creating genres'
 
 mpb = Genre.create!(name: 'MPB')
 sertanejo = Genre.create!(name: 'Sertanejo')
 rock = Genre.create!(name: 'Rock')
-tecnobrega = Genre.create!(name: 'Tecnobrega')
 folk = Genre.create!(name: 'Folk')
 jazz = Genre.create!(name: 'Jazz')
+classica = Genre.create!(name: 'Classica')
+criancas = Genre.create!(name: 'Crianças')
+fuleragem = Genre.create!(name: 'Fuleragem')
+forro = Genre.create!(name: 'Forró')
+eletronica = Genre.create!(name: 'Eletrônica')
+samba = Genre.create!(name: 'Samba')
 
 
 puts 'Creating new projects...'
 
-encanta = Project.create!(
-  artist: ian,
-  category: banquinho,
-  name: "Ian Canta & Encanta",
-  description: "Todo charme e elegância do artista Ian em formato intimista",
-  city: "São Paulo",
-  state: "SP",
-  price: 500,
-  video: "https://www.youtube.com/embed/nSbzyEJ8X9E",
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551461833/born.jpg",
-)
-
-casar = Project.create!(
-  artist: ian,
-  category: casamento,
-  name: "Case e se arrependa",
-  description: "O projeto perfeito para destruir sua festa de casamento",
-  city: "Rio de Janeiro",
-  state: "RJ",
-  price: 2500,
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551462046/bandwedding.jpg",
-  video: "https://www.youtube.com/embed/U2fQS2W1iSQ",
-)
-
-
-balanca = Project.create!(
-  artist: ian,
-  category: pauleira,
-  name: "Balança a cabeça com o Ian",
-  description: "Ian inovando com toda sua rebeldia e irreverência",
-  city: "Marília",
-  state: "SP",
-  price: 1500,
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551297724/metal.jpg",
-  video: "https://www.youtube.com/embed/TcBYBukjkBY",
-)
-
-luciana_billie = Project.create!(
-  artist: luciana,
-  category: dancar,
-  name: "Tributo a Billie Holiday",
+billie = Project.create!(
+  artist: lucianauser,
+  category: tributo,
+  name: "Tributo Billie Holiday",
   description: "Luciana Pires é cantora e compositora. Traz em seu trabalho o melhor da música brasileira e do jazz, com toques atuais, acompanhando a evolução da música e se aproximando cada vez mais do público através de suas letras e melodias. Sempre com amor e respeito à arte.",
   city: "São Paulo",
   state: "SP",
-  price: 8000,
+  price: 2000,
   remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551452069/billie.png",
-  video: "https://www.youtube.com/embed/WJCpDmpiobk",
+  video: "https://www.youtube.com/watch?v=fW_wxq5HSUM",
   )
 
-luciana_amy = Project.create!(
-  artist: luciana,
-  category: cover,
-  name: "Amy Winehouse Cover",
+amy = Project.create!(
+  artist: lucianauser,
+  category: tributo,
+  name: "Tributo Amy Winehouse",
   description: "Luciana Pires é cantora e compositora. Traz em seu trabalho o melhor da música brasileira e do jazz, com toques atuais, acompanhando a evolução da música e se aproximando cada vez mais do público através de suas letras e melodias. Sempre com amor e respeito à arte.Atualmente, a cantora possui um projeto de show autoral que evidencia a música no Brasil, o Jazz e o Soul. Possui também um show de Jazz e Música Brasileira e os projetos: Tributo à Amy Winehouse e Tributo à Billie Holiday.",
   city: "São Paulo",
   state: "SP",
-  price: 10000,
+  price: 2000,
   remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551453005/amy.png",
-  video: "https://www.youtube.com/embed/jra9ZxVWAX0",
+  video: "https://www.youtube.com/watch?v=jra9ZxVWAX0",
   )
 
-general_style = Project.create!(
+jazzluciana = Project.create!(
+  artist: lucianauser,
+  category: casamento,
+  name: "Jazz e Música Brasileira",
+  description: "Luciana Pires é cantora e compositora. Traz em seu trabalho o melhor da música brasileira e do jazz, com toques atuais, acompanhando a evolução da música e se aproximando cada vez mais do público através de suas letras e melodias. Sempre com amor e respeito à arte.Atualmente, a cantora possui um projeto de show autoral que evidencia a música no Brasil, o Jazz e o Soul. Possui também um show de Jazz e Música Brasileira e os projetos: Tributo à Amy Winehouse e Tributo à Billie Holiday.",
+  city: "São Paulo",
+  state: "SP",
+  price: 2000,
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552064249/jazz.png",
+  video: "https://www.youtube.com/watch?v=CPhRXPUb-kI",
+  )
+
+luciana = Project.create!(
+  artist: lucianauser,
+  category: autoral,
+  name: "Luciana Pires",
+  description: "Luciana Pires é cantora e compositora. Traz em seu trabalho o melhor da música brasileira e do jazz, com toques atuais, acompanhando a evolução da música e se aproximando cada vez mais do público através de suas letras e melodias. Sempre com amor e respeito à arte.Atualmente, a cantora possui um projeto de show autoral que evidencia a música no Brasil, o Jazz e o Soul. Possui também um show de Jazz e Música Brasileira e os projetos: Tributo à Amy Winehouse e Tributo à Billie Holiday.",
+  city: "São Paulo",
+  state: "SP",
+  price: 2000,
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552064679/lucianaautoral.png",
+  video: "https://www.youtube.com/watch?v=EfWA22_t4e4",
+  )
+
+general = Project.create!(
   artist: kreo,
-  category: fuleragem,
+  category: banda,
   name: "General Style",
   description: "A banda GENERAL STYLE vem com um repertório inusitado, uma mistura de estilos e sons que fazem a cabeça de todos e todas!
   Temos alguns modelos de apresentação, de a acordo com seu estabelecimento ou evento!
@@ -169,138 +165,97 @@ general_style = Project.create!(
   GENERAL STYLE - onde começa a sua festa!",
   city: "Marília",
   state: "SP",
-  price: 1200,
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551460360/general.jpg",
-  video: "https://www.youtube.com/embed/31saoUyKcWA",
+  price: 2000,
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552065392/generalstyle.jpg",
+  video: "https://www.youtube.com/watch?v=z74fWHWEup8",
   )
 
-onemanband = Project.create!(
+imaginaria = Project.create!(
   artist: kreo,
-  category: solo,
-  name: "One Man Band!",
+  category: banquinho,
+  name: "Krêo Fidélis e a Banda Imaginária",
   description: "Banda Imaginária é o projeto mais recente do músico, que já fez história e histórias com bandas, solos, gravações autorais e covers cada vez mais reconhecidos.
   Vem aí um grande projeto em uma fase em que ele busca achar na simplicidade o que buscou a carreira toda: viver de música com muito prazer em criar e tocar. Conheça mais sobre Kreo Fidelis.",
   city: "Marília",
   state: "SP",
-  price: 500,
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551461454/one_man.jpg",
-  video: "https://www.youtube.com/embed/EgbAqk24kQY",
+  price: 2000,
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552051282/p94urp0ylxywhesoupd6.jpg",
+  video: "https://www.youtube.com/watch?v=lPqQH7sijlU",
   )
 
-karaoke = Project.create!(
-  artist: ian,
-  category: solo,
-  name: "Karaoke com o Ianzão!",
-  description: "Karaoke com Ianzão é diversão garantida! Mostre todo seu potencial vocal com uma banda ao vivo! Contrate o Ian e ele toca pra você!",
+tio = Project.create!(
+  artist: kreo,
+  category: infantil,
+  name: "Tio Fidélis",
+  description: "Tio Fidélis é um projeto onde a música é o fator crucial para o desenvolvimento e adaptação da rotina da criança em ambientes escolares",
+  city: "Marília",
+  state: "SP",
+  price: 2000,
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552067218/tiofideliscapa.jpg",
+  video: "https://www.youtube.com/watch?v=BpPI7Gcs22w",
+  )
+
+kreo_autoral = Project.create!(
+  artist: kreo,
+  category: autoral,
+  name: "Krêo Fidélis",
+  description: "Sua primeira experiência com um instrumento musical foi acompanhado de um cavaco. Para Krêo, a música está presente em sua vida como uma “simbiose”. “Eu me considero músico desde o nascimento. O ambiente familiar sempre me proporcionou o contato com a música”, afirmou.",
   city: "São Paulo",
   state: "SP",
   price: 2000,
-  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1551899527/karaoke.jpg",
-  video: "https://www.youtube.com/embed/8lELdGWHlDg",
+  remote_photo_url: "https://res.cloudinary.com/dwzrkks74/image/upload/v1552067735/kreocapa.jpg",
+  video: "https://www.youtube.com/watch?v=8aKw-nNu-ss",
   )
+
 
 puts 'Creating project genres'
 
-ProjectGenre.create!(genre: rock, project: balanca)
-ProjectGenre.create!(genre: mpb, project: balanca)
-ProjectGenre.create!(genre: folk, project: balanca)
+ProjectGenre.create!(genre: jazz, project: billie)
 
-ProjectGenre.create!(genre: folk, project: encanta)
-ProjectGenre.create!(genre: tecnobrega, project: encanta)
-ProjectGenre.create!(genre: sertanejo, project: encanta)
+ProjectGenre.create!(genre: jazz, project: amy)
+ProjectGenre.create!(genre: folk, project: amy)
 
-ProjectGenre.create!(genre: mpb, project: casar)
-ProjectGenre.create!(genre: rock, project: casar)
-ProjectGenre.create!(genre: sertanejo, project: casar)
+ProjectGenre.create!(genre: mpb, project: jazzluciana)
+ProjectGenre.create!(genre: folk, project: jazzluciana)
+ProjectGenre.create!(genre: jazz, project: jazzluciana)
 
-ProjectGenre.create!(genre: jazz, project: luciana_billie)
+ProjectGenre.create!(genre: jazz, project: luciana)
+ProjectGenre.create!(genre: mpb, project: luciana)
 
-ProjectGenre.create!(genre: jazz, project: luciana_amy)
+ProjectGenre.create!(genre: rock, project: general)
+ProjectGenre.create!(genre: sertanejo, project: general)
+ProjectGenre.create!(genre: mpb, project: general)
+ProjectGenre.create!(genre: eletronica, project: general)
+ProjectGenre.create!(genre: fuleragem, project: general)
 
-ProjectGenre.create!(genre: rock, project: general_style)
-ProjectGenre.create!(genre: sertanejo, project: general_style)
-ProjectGenre.create!(genre: mpb, project: general_style)
-ProjectGenre.create!(genre: tecnobrega, project: general_style)
+ProjectGenre.create!(genre: mpb, project: imaginaria)
+ProjectGenre.create!(genre: folk, project: imaginaria)
+ProjectGenre.create!(genre: rock, project: imaginaria)
 
-ProjectGenre.create!(genre: mpb, project: onemanband)
-ProjectGenre.create!(genre: rock, project: onemanband)
-ProjectGenre.create!(genre: folk, project: onemanband)
+ProjectGenre.create!(genre: criancas, project: tio)
 
-ProjectGenre.create!(genre: mpb, project: karaoke)
-ProjectGenre.create!(genre: rock, project: karaoke)
+ProjectGenre.create!(genre: mpb, project: kreo_autoral)
+ProjectGenre.create!(genre: folk, project: kreo_autoral)
+ProjectGenre.create!(genre: samba, project: kreo_autoral)
+
+
 
 puts 'Creating events'
 
 Event.create!(
-  datetime: "2019-05-15 13:00:00",
-  description: "Quero um pro meu churrasco",
-  project: balanca,
+  datetime: "2019-03-13 16:20:00",
+  description: "Venham tocar no último Open Mic do Batch#211",
+  project: amy,
   user: aldo,
-  )
-
-Event.create!(
-  datetime: "2019-07-20 18:00:00",
-  description: "Gostaria que tocasse na minha bodas de ouro!",
-  project: luciana_amy,
-  user: ian,
-  )
-
-Event.create!(
-  datetime: "2019-06-10 14:00:00",
-  description: "Adorei o projeto, vc faria metade do preço?",
-  project: general_style,
-  user: ian,
-  )
-
-Event.create!(
-  datetime: "2019-05-08 20:00:00",
-  description: "Quero você de qualquer jeito!",
-  project: onemanband,
-  user: ian,
   )
 
 puts 'Creating reviews'
 
 Review.create!(
-  project: balanca,
-  content: "Essa Coca é fanta... de Rock não tem nada!",
-  user: aldo,
-  rate: 5,
-  )
-
-Review.create!(
-  project: encanta,
-  content: "O Ian é um gato, se puder me ligar depois te agradeço!",
-  rate: 4,
-  user: aldo,
-  )
-
-Review.create!(
-  project: encanta,
+  project: luciana,
   content: "Lindo, emocionante e sensível",
-  rate: 3,
+  rate: 5,
   user: aldo,
-  )
-
-Review.create!(
-  project: casar,
-  content: "Animou muito meu casamento, agora é trabaiar",
-  user: aldo,
-  rate: 1,
-  )
-
-Review.create!(
-  project: onemanband,
-  content: "Perfeito!!!!!",
-  user: ian,
-  rate: 0,
-  )
-
-Review.create!(
-  project: general_style,
-  content: "Detestei vocês não recomendo! Horrível... horrível",
-  user: ian,
-  rate: 2,
   )
 
 puts 'done!'
